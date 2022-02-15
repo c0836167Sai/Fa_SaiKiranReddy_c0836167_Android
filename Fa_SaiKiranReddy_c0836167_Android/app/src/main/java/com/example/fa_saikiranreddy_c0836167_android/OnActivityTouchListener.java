5de89da0bf70796ad318be933632e1a76c03bb84
SAI KIRAN REDDY POCHAMPALLY <c0836167@mylambton.ca>
2022-02-15 14:21:30 -0500

    Delete Fa_SaiKiranReddy_c0836167_Android directory

--- Fa_SaiKiranReddy_c0836167_Android/app/src/main/java/com/example/fa_saikiranreddy_c0836167_android/OnActivityTouchListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.fa_saikiranreddy_c0836167_android;
-
-import android.view.MotionEvent;
-
-public interface OnActivityTouchListener {
-    void getTouchCoordinates(MotionEvent ev);
-}
