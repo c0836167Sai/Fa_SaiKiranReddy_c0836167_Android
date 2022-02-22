4145e33425c5443509aea3bf191197be4d58f267
SAI KIRAN REDDY POCHAMPALLY <c0836167@mylambton.ca>
2022-02-22 18:45:30 -0500

    Delete Fa_SaiKiranReddy_c0836167_Android directory

--- Fa_SaiKiranReddy_c0836167_Android/app/src/main/java/com/example/fa_saikiranreddy_c0836167_android/AppDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.fa_saikiranreddy_c0836167_android;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {FavoritePlace.class}, version = 1)
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract FavoritePlaceDao placeDao();
-}
